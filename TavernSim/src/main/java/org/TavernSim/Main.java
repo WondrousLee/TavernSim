3729ff7e8b4b40f86843e466107e9ae0f0c3e263
Marcin Cudny <55958468+WondrousLee@users.noreply.github.com>
2023-08-13 14:36:44 +0200

    Many Customers per game
    deleted unimportant file
    added many customers

--- TavernSim/src/main/java/org/TavernSim/Main.java
+++ TavernSim/src/main/java/org/TavernSim/Main.java
@@ -1,15 +1,31 @@
 package org.TavernSim;
+import java.awt.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
 
-
-
-
-        Order newOrder = new Order();
         System.out.println("------------------- Hello and welcome to Tavern! -------------------");
-        newOrder.orderPlaced();
+            Scanner userInputUi = new Scanner(System.in);
+            boolean endgame = false;
+            
+            do {
+                System.out.println("Tavern Player UI:");
+                System.out.println("1: Get new customer in line");
+                System.out.println("2: Close tavern");
+                int MenuSelector = Integer.parseInt(userInputUi.nextLine());
+                switch (MenuSelector) {
+                    case 1 -> {
+                        Order newOrder = new Order();
+                        newOrder.orderPlaced();
+                    }
+                    case 2 -> {
+                        System.out.println("Day 1 finished");
+                        endgame = true;
+                    }
+
+                }
+        } while(!endgame);
     }
 }
