8de3e3011b7835aeee644b3c1fc33b55bc134c98
Marcin Cudny <55958468+WondrousLee@users.noreply.github.com>
2023-08-13 10:01:19 +0200

    Working small steps on bartering

--- TavernSim/src/main/java/org/TavernSim/Main.java
+++ TavernSim/src/main/java/org/TavernSim/Main.java
@@ -3,9 +3,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Client newClient = new Client();
+        Order newOrder = new Order();
         System.out.println("------------------- Hello and welcome to Tavern! -------------------");
-        newClient.ClientComes();
-        newClient.Bargain();
+        newOrder.orderPlaced();
     }
 }
--- TavernSim/src/main/java/org/TavernSim/Order.java
+++ TavernSim/src/main/java/org/TavernSim/Order.java
@@ -1,20 +1,34 @@
 package org.TavernSim;
-
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Order {
-    public int priceMin;
-    public int priceMax;
     int neededMaterials;
+
+
     String orderName = "Beer"; //static for now
+    String name = "Bob";
+    String MessageHi = "Hi! My names is ";
+    String MessageGetMe = ", can you get me: ";
+    String MessageBargainFailed = "Sorry no can do."; //delete it later
+
+    //Bargain Related Dialogs, later there will be more variations etc.
+    String ClientBargainTooExpensive = "I will not pay that amount of money for this drink! This price is outrageous!"; //0
+    String ClientBargainClose = "Can you give me lower price?"; //1
+    String ClientBargainCloser = "This price is almost ideal, can you lower it a little bit more tho?" ; //2
+    String ClientTooLowPrice = "I can't give you so little for that drink!"; //3
+    String MessageBargainEndOnFail = "I can't deal with you!";
+    String MessageBargainSucceeded = "Yeah, it seems fair. There you go.";
+
+
 
     Random rand = new Random();
-    //upperbound will not be the same for every drink
     //if we consider that probably not every drink needs as much resources
     //the upperbound will scale down on the resource number?
 
-    public void orderPlaced(){ //ugly stuff. remove it later
+    public void orderPlaced(){
+        System.out.println("YOU: What can i get you?");
+        System.out.println(name + ":" + MessageHi + name + MessageGetMe + orderName);
+
         int priceMin = rand.nextInt(20 - 10) + 10;
         int priceInterval = rand.nextInt(10 - 5) + 5;
         int priceMax = priceMin+priceInterval;
@@ -26,5 +40,29 @@ public class Order {
         System.out.println("price is:" + priceMin + "-" + priceMax);
         System.out.println("required materials:" + neededMaterials);
 
+        Scanner userInput = new Scanner(System.in);
+        int bargainAttempts = 0;
+        boolean finishBargain;
+        do { //Get this into other method, tip by Zaszczynski, should prevent leaks and make code a lil cleaner
+            int playerProposal = Integer.parseInt(userInput.nextLine());
+
+            if (playerProposal >= priceMin && playerProposal <= priceMax) {
+                System.out.println(MessageBargainSucceeded);
+                finishBargain = true;
+            } else {
+                finishBargain = false;
+                bargainAttempts++;
+
+                if (bargainAttempts > 3){
+                    System.out.println("-=| Bargain Failed! |=-"); //After changing status from "lower" to for example "just little bit lower" reset fail count
+                    System.out.println(MessageBargainEndOnFail);
+                    break;
+                }
+                else {
+                    System.out.println(MessageBargainFailed); //this should later print out if we want lower value or higher.
+                }
+            }
+        }
+        while(!finishBargain);
     }
 }
